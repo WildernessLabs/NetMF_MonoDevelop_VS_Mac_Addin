2f91db26832eb9d4cb4848e05c1421ffaf8007c4
Jérémie Laval <jeremie.laval@gmail.com>
2017-08-21 01:23:13 -0400

    Fully switch to MonoDevelop.Addins style
    Use assembly attribute to specify addin information and import NETMF files directly from csproj

---
+++ MonoDevelop.MicroFramework/Properties/AddinInfo.cs
@@ -0,0 +1,12 @@
+﻿
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin ("MicroFramework",
+                  Namespace = "MonoDevelop",
+                  Version = "0.6.4",
+                  Category = "IDE extensions")]
+
+[assembly: AddinName ("MicroFramework")]
+[assembly: AddinDescription ("Provides support for .Net MicroFramework developing, deploying and debugging.")]
+[assembly: AddinAuthor ("David Karlaš & Jérémie Laval")]
