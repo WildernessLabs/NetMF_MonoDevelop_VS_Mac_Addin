e55e7f691e9e6acea46b1d48b39790692efa4fcb
bryan costanich <b@wowzer.net>
2018-01-14 18:34:37 -0800

    Bumped addin version.

--- MonoDevelop.MicroFramework/Properties/AddinInfo.cs
+++ MonoDevelop.MicroFramework/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 
 [assembly: Addin ("MicroFramework",
                   Namespace = "MonoDevelop",
-                  Version = "1.0.1",
+                  Version = "1.0.2",
                   Category = "IDE extensions")]
 
 [assembly: AddinName ("MicroFramework")]
