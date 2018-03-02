f9b8c06768352156b3dc24c27497f26c64f961b0
Alex Corrado <alexander.corrado@gmail.com>
2018-03-02 00:44:48 -0500

    Even better fixup to project fixups
    These need to happen on the unevaluated MSBuildProject.
    No idea how it ever worked before (e.g. in 876cc98 or even before that),
    but it seems to work now. Hooray!
    
    Fixes https://github.com/WildernessLabs/NetMF_MonoDevelop_VS_Mac_Addin/issues/12

---
+++ VS Addin/MonoDevelop.MicroFramework/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MonoDevelop.Projects.MSBuild;
+
+namespace MonoDevelop.MicroFramework
+{
+		static class Extensions
+		{
+			public static void AddImportIfMissing (this MSBuildProject project, string name, string condition = null)
+			{
+				var existing = project.GetImport (name, condition);
+				if (existing == null)
+					project.AddNewImport (name, condition);
+			}
+
+			public static void RemoveProperty (this MSBuildProject project, string name)
+			{
+				foreach (var group in project.PropertyGroups)
+					group.RemoveProperty (name);
+			}
+        }
+ }
